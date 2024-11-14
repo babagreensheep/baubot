88bfcc10a3c38833e092b79f809d4f91119e3729
Shaun See Tow <stshaun@gmail.com>
2024-11-15 06:36:33 +0800

    utils for testing

---
+++ baubot-utils/build.rs
@@ -0,0 +1,43 @@
+//! Build script to generate env variables
+
+use std::io::Write;
+
+fn main() {
+    let manifest_path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let secrets_path = manifest_path.join("secrets.env");
+
+    // Initialize env secrets
+    dotenvy::from_path(&secrets_path).expect(&format!(
+        "build requires a secrets file at {}",
+        secrets_path.clone().to_string_lossy()
+    ));
+
+    // Create build file
+    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let secrets_file_path = out_dir.join("secrets.rs");
+    let secrets_file = std::fs::File::create(&secrets_file_path).unwrap();
+    let mut file_stream = std::io::BufWriter::new(secrets_file);
+    println!(
+        "cargo:warning=\"secrets file generated at: {}\"",
+        secrets_file_path.to_string_lossy()
+    );
+
+    // Generate keys
+    for (key, value) in std::env::vars().filter_map(|(key, value)| {
+        let key = key.strip_prefix("TELE_")?;
+        Some((key.to_string(), value))
+    }) {
+        match value.parse::<u128>() {
+            Ok(value) => writeln!(file_stream, "pub const {key}: u128 = {value};").unwrap(),
+            Err(_) => writeln!(file_stream, "pub const {key}: &str = \"{value}\";").unwrap(),
+        }
+    }
+
+    // Flush stream
+    file_stream.flush().unwrap();
+
+    // Force rerun
+    let build_script = manifest_path.join("build.rs");
+    println!("cargo:rerun-if-changed={}", build_script.to_string_lossy());
+    println!("cargo:rerun-if-changed={}", secrets_path.to_string_lossy());
+}
---
+++ baubot-utils/src/lib.rs
@@ -0,0 +1,26 @@
+//! Bindings to environment secrets for use across the workspace. Because accessing env secrets
+//! sucks donkey balls.
+
+include!(concat!(env!("OUT_DIR"), "/secrets.rs"));
+
+/// Call this function to initialize loggers etc.. Env secrets are consts.
+pub fn init() {
+    static INIT: std::sync::Once = std::sync::Once::new();
+    INIT.call_once(|| {
+        // Initialise logger
+        env_logger::Builder::new()
+            .filter_level(log::LevelFilter::Trace)
+            .filter_module("teloxide", log::LevelFilter::Off)
+            .filter_module("reqwest", log::LevelFilter::Off)
+            .parse_env("LOG_LEVEL")
+            .init();
+
+        log::info!("Init complete.");
+    });
+}
+
+#[test]
+fn test() {
+    init();
+    log::info!("{TELOXIDE_TOKEN}");
+}
